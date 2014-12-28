86acc419e1a7e3b206074a04da47af3eceddcf54
Jørgen Wang Svendsen <jorgen.wang.svendsen@gmail.com>
2014-12-28 23:21:54 +0100

    implemented json loading

--- Core/JsonLocalizationLoader.cs
+++ Core/JsonLocalizationLoader.cs
@@ -1,9 +1,22 @@
-﻿using System;
+﻿using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
 
 namespace Core {
     public class JsonLocalizationLoader : ILocalizationLoader {
         public LoadedLocalization Load(string filePath) {
-            throw new NotImplementedException();
+            LocalizationPersistance persistance = null;
+            using (var fs = File.Open(filePath,
+                                      FileMode.Open))
+            using (var sr = new StreamReader(fs))
+            using (var jr = new JsonTextReader(sr)) {
+                var serializer = new JsonSerializer();
+                persistance = serializer.Deserialize<LocalizationPersistance>(jr);
+            }
+
+            return new LoadedLocalization(filePath,
+                                          persistance.Languages.ToList(),
+                                          persistance.HistoryEntries.ToList());
         }
     }
 
