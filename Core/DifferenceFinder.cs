b2e0925b1a84d74a07416d9e2cb5180cc5a1c5f0
Jørgen Wang Svendsen <jorgen.wang.svendsen@gmail.com>
2014-12-29 16:57:43 +0100

    refactored difference finder

--- Core/DifferenceFinder.cs
+++ Core/DifferenceFinder.cs
@@ -7,41 +7,35 @@ namespace Core {
     public class DifferenceFinder : IDifferenceFinder {
         public string Changes(ILoadedLocalization previousLocalization,
                               ILocalization currentLocalization) {
-            string difference = string.Empty;
-            
-            difference += AreaDifference(previousLocalization.Areas,
-                             currentLocalization.RetriveAreas()
-                                                .ToList());
-
-            difference += LanguageDifference(previousLocalization.Languages,
-                                             currentLocalization.RetriveLanguages()
-                                                                .ToList());
-            return difference;
-        }
-
-        private string LanguageDifference(List<Language> previousLanguages,
-                                          List<Language> currentLanguages) {
-            var previousLanguagesDictionary = previousLanguages.ToDictionary(c => c.Id);
-            var currentLanguagesDictionary = currentLanguages.ToDictionary(c => c.Id);
-
             var builder = new StringBuilder();
 
-            int added = currentLanguages.Count(c => !previousLanguagesDictionary.ContainsKey(c.Id));
-            if (added != 0)
-                builder.AppendLine(string.Format(@"{0} language(s) have been added.",
-                                             added));
+            builder.AppendLine(Difference(previousLocalization.Areas,
+                                          currentLocalization.RetriveAreas()
+                                                             .ToList(),
+                                          a => a.Id,
+                                          AreAreasEqual,
+                                          "area"));
 
-            int removed = previousLanguages.Count(c => !currentLanguagesDictionary.ContainsKey(c.Id));
-            if (removed != 0)
-                builder.AppendLine(string.Format(@"{0} language(s) have been removed.",
-                                                 removed));
-            int changed = currentLanguages.Where(c => previousLanguagesDictionary.ContainsKey(c.Id))
-                                          .Select(c => c.Id)
-                                          .Count(c => !AreLanguagesEqual(previousLanguagesDictionary[c],
-                                                                         currentLanguagesDictionary[c]));
-            if (changed != 0)
-                builder.AppendLine(string.Format(@"{0} language(s) have been changed.",
-                                                 changed));
+            builder.AppendLine(Difference(previousLocalization.Keys,
+                                          currentLocalization.RetriveKeys()
+                                                             .ToList(),
+                                          k => k.Id,
+                                          AreKeysEqual,
+                                          "key"));
+
+            builder.AppendLine(Difference(previousLocalization.Texts,
+                                          currentLocalization.RetriveTexts()
+                                                             .ToList(),
+                                          t => t.Id,
+                                          AreTextsEqual,
+                                          "text"));
+
+            builder.AppendLine(Difference(previousLocalization.Languages,
+                                          currentLocalization.RetriveLanguages()
+                                                             .ToList(),
+                                          l => l.Id,
+                                          AreLanguagesEqual,
+                                          "language"));
             return builder.ToString();
         }
 
@@ -49,49 +43,64 @@ namespace Core {
                                        Language y) {
             if (x == y)
                 throw new NotSupportedException("This method compares objects by value");
-            if (x.Id == y.Id && x.IsoName == y.IsoName && x.DisplayName == y.DisplayName)
-                return true;
-
-            return false;
-        }
-
-        private string AreaDifference(List<Area> previousArea,
-                                      List<Area> currentArea) {
-            var previousAreaDictionary = previousArea.ToDictionary(c => c.Id);
-            var currentAreaDictionary = currentArea.ToDictionary(c => c.Id);
-
-            var builder = new StringBuilder();
-
-            int added = currentArea.Count(c => !previousAreaDictionary.ContainsKey(c.Id));
-            if (added != 0)
-                builder.AppendLine(string.Format(@"{0} area(s) have been added.",
-                                                 added));
-
-            int removed = previousArea.Count(c => !currentAreaDictionary.ContainsKey(c.Id));
-            if (removed != 0)
-                builder.AppendLine(string.Format(@"{0} area(s) have been removed.",
-                                                 removed));
-
-            int changes = currentArea.Where(c => previousAreaDictionary.ContainsKey(c.Id))
-                                     .Select(c => c.Id)
-                                     .Count(c => !AreAreasEqual(previousAreaDictionary[c],
-                                                                currentAreaDictionary[c]));
-            if (changes!=0) {
-                builder.AppendLine(string.Format(@"{0} area(s) have been changed.",
-                                                 changes));
-            }
-
-            return builder.ToString();
+            return x.Id == y.Id && x.IsoName == y.IsoName && x.DisplayName == y.DisplayName;
         }
 
         private bool AreAreasEqual(Area x,
                                    Area y) {
             if (x == y)
                 throw new NotSupportedException("This method compares objects by value");
-            if (x.Id == y.Id && x.Name == y.Name)
-                return true;
+            return x.Id == y.Id && x.Name == y.Name;
+        }
 
-            return false;
+        private bool AreKeysEqual(LocalizationKey x,
+                                  LocalizationKey y) {
+            if (x == y)
+                throw new NotSupportedException("This metod compares by value");
+            return x.Id == y.Id && x.AreaId == y.AreaId && x.Key == y.Key;
+        }
+
+        private bool AreTextsEqual(LocalizedText x,
+                                   LocalizedText y) {
+            if (x == y)
+                throw new NotImplementedException("This method compares by value");
+
+            return x.Id == y.Id && x.Value == y.Value && x.KeyId == y.KeyId && x.LanguageId == y.LanguageId;
+        }
+
+        private string Difference<T>(List<T> previous,
+                                     List<T> current,
+            Func<T,Guid> toId,
+            Func<T,T,bool> areEqual,
+                                     string type) {
+            var previousDictionary = previous.ToDictionary(toId);
+            var currentDictionary = current.ToDictionary(toId);
+
+            var builder = new StringBuilder();
+
+            int added = current.Count(c => !previousDictionary.ContainsKey(toId(c)));
+            if (added != 0)
+                builder.AppendLine(string.Format(@"{0} {1}(s) have been added.",
+                                                 added,
+                                                 type));
+
+            int removed = previous.Count(c => !currentDictionary.ContainsKey(toId(c)));
+            if (removed != 0)
+                builder.AppendLine(string.Format(@"{0} {1}(s) have been removed.",
+                                                 removed,
+                                                 type));
+
+            int changes = current.Where(c => previousDictionary.ContainsKey(toId(c)))
+                                     .Select(toId)
+                                     .Count(c => !areEqual(previousDictionary[c],
+                                                                currentDictionary[c]));
+            if (changes != 0) {
+                builder.AppendLine(string.Format(@"{0} {1}(s) have been changed.",
+                                                 changes,
+                                                 type));
+            }
+
+            return builder.ToString();
         }
     }
 
