76f916f33b68d4ee2fd06433c94390c6a96919b7
Jørgen Wang Svendsen <jorgen.wang.svendsen@gmail.com>
2014-12-29 16:23:31 +0100

    added difference finder code for area

--- Core/DifferenceFinder.cs
+++ Core/DifferenceFinder.cs
@@ -5,13 +5,17 @@ using System.Text;
 
 namespace Core {
     public class DifferenceFinder : IDifferenceFinder {
-        public string LanguageChanges(ILoadedLocalization previousLocalization,
-                                      ILocalization currentLocalization) {
+        public string Changes(ILoadedLocalization previousLocalization,
+                              ILocalization currentLocalization) {
             string difference = string.Empty;
+            
+            difference += AreaDifference(previousLocalization.Areas,
+                             currentLocalization.RetriveAreas()
+                                                .ToList());
+
             difference += LanguageDifference(previousLocalization.Languages,
                                              currentLocalization.RetriveLanguages()
                                                                 .ToList());
-            
             return difference;
         }
 
@@ -24,7 +28,7 @@ namespace Core {
 
             int added = currentLanguages.Count(c => !previousLanguagesDictionary.ContainsKey(c.Id));
             if (added != 0)
-                builder.Append(string.Format(@"{0} language(s) have been added.",
+                builder.AppendLine(string.Format(@"{0} language(s) have been added.",
                                              added));
 
             int removed = previousLanguages.Count(c => !currentLanguagesDictionary.ContainsKey(c.Id));
@@ -33,16 +37,16 @@ namespace Core {
                                                  removed));
             int changed = currentLanguages.Where(c => previousLanguagesDictionary.ContainsKey(c.Id))
                                           .Select(c => c.Id)
-                                          .Count(c => !AreEqual(previousLanguagesDictionary[c],
-                                                                currentLanguagesDictionary[c]));
+                                          .Count(c => !AreLanguagesEqual(previousLanguagesDictionary[c],
+                                                                         currentLanguagesDictionary[c]));
             if (changed != 0)
                 builder.AppendLine(string.Format(@"{0} language(s) have been changed.",
                                                  changed));
             return builder.ToString();
         }
 
-        private bool AreEqual(Language x,
-                              Language y) {
+        private bool AreLanguagesEqual(Language x,
+                                       Language y) {
             if (x == y)
                 throw new NotSupportedException("This method compares objects by value");
             if (x.Id == y.Id && x.IsoName == y.IsoName && x.DisplayName == y.DisplayName)
@@ -50,10 +54,49 @@ namespace Core {
 
             return false;
         }
+
+        private string AreaDifference(List<Area> previousArea,
+                                      List<Area> currentArea) {
+            var previousAreaDictionary = previousArea.ToDictionary(c => c.Id);
+            var currentAreaDictionary = currentArea.ToDictionary(c => c.Id);
+
+            var builder = new StringBuilder();
+
+            int added = currentArea.Count(c => !previousAreaDictionary.ContainsKey(c.Id));
+            if (added != 0)
+                builder.AppendLine(string.Format(@"{0} area(s) have been added.",
+                                                 added));
+
+            int removed = previousArea.Count(c => !currentAreaDictionary.ContainsKey(c.Id));
+            if (removed != 0)
+                builder.AppendLine(string.Format(@"{0} area(s) have been removed.",
+                                                 removed));
+
+            int changes = currentArea.Where(c => previousAreaDictionary.ContainsKey(c.Id))
+                                     .Select(c => c.Id)
+                                     .Count(c => !AreAreasEqual(previousAreaDictionary[c],
+                                                                currentAreaDictionary[c]));
+            if (changes!=0) {
+                builder.AppendLine(string.Format(@"{0} area(s) have been changed.",
+                                                 changes));
+            }
+
+            return builder.ToString();
+        }
+
+        private bool AreAreasEqual(Area x,
+                                   Area y) {
+            if (x == y)
+                throw new NotSupportedException("This method compares objects by value");
+            if (x.Id == y.Id && x.Name == y.Name)
+                return true;
+
+            return false;
+        }
     }
 
     public interface IDifferenceFinder {
-        string LanguageChanges(ILoadedLocalization previousLocalization,
-                               ILocalization currentLocalization);
+        string Changes(ILoadedLocalization previousLocalization,
+                       ILocalization currentLocalization);
     }
 }
--- CoreTests/DifferenceFinderTests.cs
+++ CoreTests/DifferenceFinderTests.cs
@@ -11,15 +11,33 @@ namespace CoreTests {
         private Mock<ILocalization> _localization;
 
         private readonly Guid _languageId = Guid.Parse("{0520C019-B49D-4866-9BD6-D87751EE304B}");
+        
+        private readonly Guid _areaId = Guid.Parse("{971714A7-6C96-499F-AFEA-2544EABD30DE}");
 
         [SetUp]
         public void SetUp() {
             _loadedLocalization = new Mock<ILoadedLocalization>();
+            _loadedLocalization.Setup(c => c.Areas)
+                               .Returns(new List<Area>());
+            _loadedLocalization.Setup(c => c.Keys)
+                               .Returns(new List<LocalizationKey>());
+            _loadedLocalization.Setup(c => c.Texts)
+                               .Returns(new List<LocalizedText>());
+            _loadedLocalization.Setup(c => c.Languages)
+                               .Returns(new List<Language>());
             _localization = new Mock<ILocalization>();
+            _localization.Setup(c => c.RetriveAreas())
+                         .Returns(new List<Area>());
+            _localization.Setup(c => c.RetriveKeys())
+                         .Returns(new List<LocalizationKey>());
+            _localization.Setup(c => c.RetriveTexts())
+                         .Returns(new List<LocalizedText>());
+            _localization.Setup(c => c.RetriveLanguages())
+                         .Returns(new List<Language>());
         }
 
         [Test]
-        public void LanguageDifference_AddedOneLanguage_CorrectText() {
+        public void Changes_AddedOneLanguage_CorrectText() {
             _loadedLocalization.Setup(c => c.Languages)
                                .Returns(new List<Language>());
             _localization.Setup(c => c.RetriveLanguages())
@@ -29,14 +47,14 @@ namespace CoreTests {
 
             var differenceFinder = new DifferenceFinder();
 
-            string result = differenceFinder.LanguageChanges(_loadedLocalization.Object,
+            string result = differenceFinder.Changes(_loadedLocalization.Object,
                                                              _localization.Object);
 
             Assert.IsTrue(result.Contains("1 language(s) have been added."));
         }
 
         [Test]
-        public void LanguageDifference_RemovedOneLanguage_CorrectText() {
+        public void Changes_RemovedOneLanguage_CorrectText() {
             _loadedLocalization.Setup(c => c.Languages)
                 .Returns(new List<Language> {
                                                          CreateLanguage()
@@ -47,14 +65,14 @@ namespace CoreTests {
 
             var differenceFinder = new DifferenceFinder();
 
-            string result = differenceFinder.LanguageChanges(_loadedLocalization.Object,
+            string result = differenceFinder.Changes(_loadedLocalization.Object,
                                                              _localization.Object);
 
             Assert.IsTrue(result.Contains("1 language(s) have been removed."));
         }
 
         [Test]
-        public void LanguageDifference_ChangedOneLanguage_CorrectText() {
+        public void Changes_ChangedOneLanguage_CorrectText() {
             var previousLanguage = CreateLanguage();
             _loadedLocalization.Setup(c => c.Languages)
                                .Returns(new List<Language> {
@@ -69,18 +87,79 @@ namespace CoreTests {
 
             var differenceFinder = new DifferenceFinder();
 
-            string result = differenceFinder.LanguageChanges(_loadedLocalization.Object,
+            string result = differenceFinder.Changes(_loadedLocalization.Object,
                                                              _localization.Object);
 
             Assert.IsTrue(result.Contains("1 language(s) have been changed."));
         }
 
+        [Test]
+        public void Changes_AddedOneArea_CorrectText() {
+            _loadedLocalization.Setup(c => c.Areas)
+                               .Returns(new List<Area>());
+            _localization.Setup(c => c.RetriveAreas())
+                         .Returns(new List<Area> {
+                                                     CreateArea()
+                                                 });
 
+            var differenceFinder = new DifferenceFinder();
+
+            string result = differenceFinder.Changes(_loadedLocalization.Object,
+                                                     _localization.Object);
+
+            Assert.IsTrue(result.Contains("1 area(s) have been added."));
+        }
+
+        [Test]
+        public void Changes_RemovedOneArea_CorrectText() {
+            _loadedLocalization.Setup(c => c.Areas)
+                               .Returns(new List<Area> {
+                                                           CreateArea()
+                                                       });
+            _localization.Setup(c => c.RetriveAreas())
+                         .Returns(new List<Area>());
+
+            var differenceFinder = new DifferenceFinder();
+
+            string result = differenceFinder.Changes(_loadedLocalization.Object,
+                                                     _localization.Object);
+
+            Assert.IsTrue(result.Contains("1 area(s) have been removed."));
+        }
+
+        [Test]
+        public void Changes_ChangedOneArea_CorrectText() {
+            var previous = CreateArea();
+            var current = CreateArea();
+            current.Name = "test";
+
+            _loadedLocalization.Setup(c => c.Areas)
+                               .Returns(new List<Area> {
+                                                           previous
+                                                       });
+            _localization.Setup(c => c.RetriveAreas())
+                         .Returns(new List<Area> {
+                                                     current
+                                                 });
+
+            var differenceFinder = new DifferenceFinder();
+
+            string result = differenceFinder.Changes(_loadedLocalization.Object,
+                                                     _localization.Object);
+
+            Assert.IsTrue(result.Contains("1 area(s) have been changed."));
+        }
 
         private Language CreateLanguage() {
             return new Language {
                                     Id = _languageId
                                 };
+        }
+
+        private Area CreateArea() {
+            return new Area {
+                                Id = _areaId
+                            };
         }
     }
 }
